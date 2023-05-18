caa2d7f61d92007d4f80d86fa934f26812a365bd
Takashi Mori <kanetugu2015@gmail.com>
2023-05-19 06:36:35 +0900

    add install.bash

---
+++ install.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -d plugin/plugin-srcs ]
+then
+    :
+else
+    echo "ERROR: can not find submodule plugin"
+    echo "git pull"
+    echo "git submodule update --init --recursive"
+    exit 1
+fi
+
+echo "INFO: DOWNLOADING TB3 MODEL"
+
+if [ -d tb3/Assets/Model/Hakoniwa/Robots/BurgerBot_Model ]
+then
+    echo "INFO: TB3 MODEL IS ALREADY INSTALLED."
+else
+    wget https://github.com/toppers/hakoniwa-unity-tb3model/releases/download/model-v1.0.0/TB3Burgar_01.fbx
+    mv TB3Burgar_01.fbx tb3/Assets/Model/Hakoniwa/Robots/BurgerBot_Model/
+
+    echo "INFO: COPYING TB3Assets to hakoniwa plugin(plugin/plugin-srcs/Assets/)"
+    cp -rp tb3/Assets/* plugin/plugin-srcs/Assets/
+fi
+
+cd plugin
+bash install.bash
+
+echo "DONE"
+exit 0
