e93d3ec9ea0d697d03c2ba319424100a542918bc
s-hosoai <shintaro.hosoai@gmail.com>
2023-12-18 16:21:29 +0900

    change udp to shm.
    Currently, image sensors are disabled.

--- tb3/Assets/Scripts/HakoAssets/PluggableAsset/Assets/Robot/TB3/TB3MiconConfig.cs
+++ tb3/Assets/Scripts/HakoAssets/PluggableAsset/Assets/Robot/TB3/TB3MiconConfig.cs
@@ -6,135 +6,102 @@ using UnityEngine;
 
 namespace Hakoniwa.PluggableAsset.Assets.Robot.TB3
 {
-    [System.Serializable]
-    public class TB3MiconConfigPduReader
+    public class TB3MiconConfigShmPduReader
     {
-        public string type = null;
-        public string org_name = null;
+        public string type = "";
+        public string org_name = "";
         public string name = null;
-        public string class_name = null;
+        public string class_name = "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduReader";
         public string class_path = null;
-        public string conv_class_name = null;
+        public string conv_class_name = "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduReaderConverter";
         public string conv_class_path = null;
         public int channel_id = 0;
-        public int pdu_size = 1024;
-        public string method_type = "UDP";
-        public TB3MiconConfigPduReader(string type, string org_name, string class_name, string conv_class_name, int channel_id, int pdu_size)
+        public int pdu_size = 196;
+        public string method_type = "SHM";
+        public TB3MiconConfigShmPduReader(string type, string org_name, int channel_id, int pdu_size)
         {
             this.type = type;
             this.org_name = org_name;
-            this.class_name = class_name;
-            this.conv_class_name = conv_class_name;
             this.channel_id = channel_id;
             this.pdu_size = pdu_size;
         }
     }
+    
     [System.Serializable]
-    public class TB3MiconConfigPduWriter
+    public class TB3MiconConfigShmPduWriter
     {
-        public string type = null;
-        public string org_name = null;
+        public string type = "ev3_msgs/Ev3PduSensor";
+        public string org_name = "ev3_sensor";
         public string name = null;
-        public string class_name = null;
+        public string class_name = "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter";
         public string class_path = null;
-        public string conv_class_name = null;
+        public string conv_class_name = "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter";
         public string conv_class_path = null;
-        public int channel_id = 0;
-        public int pdu_size = 1024;
-        public int write_cycle = 1;
-        public string method_type = "UDP";
-        public TB3MiconConfigPduWriter(string type, string org_name, string class_name, string conv_class_name, int channel_id, int pdu_size)
+        public int channel_id = 1;
+        public int pdu_size = 248;
+        public string method_type = "SHM";
+        public TB3MiconConfigShmPduWriter(string type, string org_name, int channel_id, int pdu_size)
         {
             this.type = type;
             this.org_name = org_name;
-            this.class_name = class_name;
-            this.conv_class_name = conv_class_name;
             this.channel_id = channel_id;
             this.pdu_size = pdu_size;
         }
     }
-
     [System.Serializable]
     public class TB3MiconConfigSettingsContainer
     {
         public string name = "micon_setting";
-        public TB3MiconConfigPduReader[] rpc_pdu_readers =
+        public TB3MiconConfigShmPduReader[] shm_pdu_readers =
         {
-            new TB3MiconConfigPduReader(
+            new TB3MiconConfigShmPduReader(
                 "geometry_msgs/Twist",
                 "cmd_vel",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduReader",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduReaderConverter",
-                  0,
-                  48
-                ),
+                0,
+                48),
         };
-        public TB3MiconConfigPduWriter[] rpc_pdu_writers =
+        public TB3MiconConfigShmPduWriter[] shm_pdu_writers =
         {
-            new TB3MiconConfigPduWriter(
+            new TB3MiconConfigShmPduWriter(
                 "sensor_msgs/JointState",
                 "joint_states",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter",
-                  1,
-                  440
-                ),
-            new TB3MiconConfigPduWriter(
+                1,
+                440),
+            new TB3MiconConfigShmPduWriter(
                 "sensor_msgs/Imu",
                 "imu",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter",
-                  2,
-                  432
-                ),
-            new TB3MiconConfigPduWriter(
+                2,
+                432),
+            new TB3MiconConfigShmPduWriter(
                 "nav_msgs/Odometry",
                 "odom",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter",
-                  3,
-                  944
-                ),
-            new TB3MiconConfigPduWriter(
+                3,
+                944),
+            new TB3MiconConfigShmPduWriter(
                 "tf2_msgs/TFMessage",
                 "tf",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter",
-                  4,
-                  320
-                ),
-            new TB3MiconConfigPduWriter(
-                "sensor_msgs/Image",
-                "image",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter",
-                  5,
-                  1229080
-                ),
-            new TB3MiconConfigPduWriter(
-                "sensor_msgs/CompressedImage",
-                "image/compressed",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter",
-                  6,
-                  1229064
-                ),
-            new TB3MiconConfigPduWriter(
-                "sensor_msgs/CameraInfo",
-                "camera_info",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter",
-                  7,
-                  580
-                ),
-            new TB3MiconConfigPduWriter(
+                4,
+                320),
+            // new TB3MiconConfigShmPduWriter(
+            //     "sensor_msgs/Image",
+            //     "image",
+            //     5,
+            //     1229080),
+            // new TB3MiconConfigShmPduWriter(
+            //     "sensor_msgs/CompressedImage",
+            //     "image/compressed",
+            //     6,
+            //     1229064),
+            // new TB3MiconConfigShmPduWriter(
+            //     "sensor_msgs/CameraInfo",
+            //     "camera_info",
+            //     7,
+            //     580),
+            new TB3MiconConfigShmPduWriter(
                 "sensor_msgs/LaserScan",
                 "scan",
-                 "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriter",
-                  "Hakoniwa.PluggableAsset.Communication.Pdu.Raw.RawPduWriterConverter",
-                  8,
-                  1800
-                ),
+                5,
+                1800),
         };
     }
 
@@ -146,11 +113,11 @@ namespace Hakoniwa.PluggableAsset.Assets.Robot.TB3
         public string GetSettings(string name)
         {
             this.settings.name = name;
-            foreach (var e in this.settings.rpc_pdu_readers)
+            foreach (var e in this.settings.shm_pdu_readers)
             {
                 e.name = name + "_" + e.org_name;
             }
-            foreach (var e in this.settings.rpc_pdu_writers)
+            foreach (var e in this.settings.shm_pdu_writers)
             {
                 e.name = name + "_" + e.org_name;
             }
@@ -162,5 +129,4 @@ namespace Hakoniwa.PluggableAsset.Assets.Robot.TB3
             return this.on;
         }
     }
-
 }
